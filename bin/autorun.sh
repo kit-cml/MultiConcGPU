0deb57569ca5cb4125a21620a85c37d84574c655
IritaSee <iga.narendra@gmail.com>
2024-10-09 15:26:27 +0900

    add sensitivity analysis run

--- bin/autorun.sh
+++ bin/autorun.sh
@@ -1,4 +1,4 @@
 cd ..
 make clean all
 cd bin
-./drug_sim -input_deck input_deck.txt -hill_file mitoxantrone/ -cvar_file CVAR/HF_male.csv
+./drug_sim -input_deck input_deck.txt -hill_file ic50_sens/ -cvar_file CVAR/HF_male.csv
